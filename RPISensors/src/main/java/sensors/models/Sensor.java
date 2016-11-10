d1af91665a63fee1d338be541109cdc3e88f6543
gjwo <grahamjwood@gmail.com>
2016-11-10 17:43:24 +0000

    More whitespace removal and tidying, sample size now used

--- RPISensors/src/main/java/sensors/models/Sensor.java
+++ RPISensors/src/main/java/sensors/models/Sensor.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import dataTypes.CircularArrayRing;
 
 /**
- * RPITank - devices.sensors
+ * RPISensor - devices.sensors
  * Created by GJWood on 18/07/2016.
  */
 public abstract class Sensor <T,S>
@@ -16,77 +16,37 @@ public abstract class Sensor <T,S>
     protected int sampleRate;
     protected int sampleSize;
 
+    /**
+     * Sensor		- Constructor
+     * @param sampleRate	- The sample rate per second
+     * @param sampleSize	- The number of samples that can be held before overwriting
+     */
     public Sensor(int sampleRate, int sampleSize)
     {
-        vals = new CircularArrayRing<T>();
+        vals = new CircularArrayRing<T>(sampleSize);
         this.sampleRate = sampleRate;
         this.sampleSize = sampleSize;
     }
 
-    public T getLatestValue()
-    {
-        return vals.get(0);
-    }
+    // Methods implemented here, shouldn't need overriding
+    public T getLatestValue(){return vals.get(0);}
+    public T getValue(int i){return vals.get(i);}
+    public int getReadingCount(){return vals.size();}
+    public void setValBias(S valBias){this.valBias = valBias;}
+    public S getValBias(){ return valBias;}
+    public void setValScaling(S valScaling){this.valScaling = valScaling;}
+    public S getValScaling(){return valScaling;}
+    public void addValue(T value){vals.add(value);}
+
+    // Methods must be implemented but which can't be done here because the types are not known
     public abstract T getAvgValue();
-
-    public T getValue(int i)
-    {
-        return vals.get(i);
-    }
-
-    public int getReadingCount()
-    {
-        return vals.size();
-    }
-
-    public void setValBias(S valBias)
-    {
-        this.valBias = valBias;
-    }
-
-    public S getValBias()
-    {
-        return valBias;
-    }
-
-    public void setValScaling(S valScaling)
-    {
-        this.valScaling = valScaling;
-    }
-
-    public S getValScaling()
-    {
-        return valScaling;
-    }
-
-    public void addValue(T value)
-    {
-        vals.add(value);
-    }
- 
     public abstract T OffsetAndScale(T value);
-
     public abstract void updateData() throws IOException;
-    
-    public void calibrate() throws IOException, InterruptedException
-    {
-    	//if required implement and override in subclass
-    }
-    public void configure() throws IOException, InterruptedException
-    {
-    	//if required implement and override in subclass
-    }
-    public void selfTest() throws IOException, InterruptedException
-    {
-    	//if required implement and override in subclass
-    }
-    public void init() throws IOException, InterruptedException
-    {
-    	//if required implement and override in subclass
-    }
-    public void printRegisters()
-    {
-    	//if required implement and override in subclass
-    }
-   
+
+    // Optional Methods
+    public void calibrate() throws IOException, InterruptedException{/*if required implement and override in subclass*/}
+    public void configure() throws IOException, InterruptedException{/*if required implement and override in subclass*/}
+    public void selfTest() throws IOException, InterruptedException{/*if required implement and override in subclass*/}
+    public void init() throws IOException, InterruptedException{/*if required implement and override in subclass*/}
+    public void printRegisters(){/*if required implement and override in subclass*/} 
 }
