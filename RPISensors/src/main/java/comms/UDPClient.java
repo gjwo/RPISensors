f2c2c2e8954af6cf7d36da58ec02be5de3ce6155
gjwo <grahamjwood@gmail.com>
2016-11-15 17:01:15 +0000

    commented code

--- RPISensors/src/main/java/comms/UDPClient.java
+++ RPISensors/src/main/java/comms/UDPClient.java
@@ -15,19 +15,23 @@ public class UDPClient
     public static void main(String args[]) throws Exception
     {
 	      BufferedReader inFromUser =
-	         new BufferedReader(new InputStreamReader(System.in));
-	      DatagramSocket clientSocket = new DatagramSocket();
-	      InetAddress IPAddress = InetAddress.getByName("localhost");
-	      byte[] sendData = new byte[1024];
-	      byte[] receiveData = new byte[1024];
-	      String sentence = inFromUser.readLine();
-	      sendData = sentence.getBytes();
-	      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-	      clientSocket.send(sendPacket);
-	      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-	      clientSocket.receive(receivePacket);
-	      String modifiedSentence = new String(receivePacket.getData());
-	      System.out.println("FROM SERVER:" + modifiedSentence);
-	      clientSocket.close();
+	         new BufferedReader(new InputStreamReader(System.in));					//set up user input
+	      DatagramSocket clientSocket = new DatagramSocket();						//create socket
+	      //InetAddress IPAddress = InetAddress.getByName("localhost");
+	      InetAddress IPAddress = InetAddress.getLocalHost();						//This machine's address - should be target?
+	      byte[] sendData = new byte[1024];											//Array for holding data to be sent
+	      byte[] receiveData = new byte[1024];										//Array for holding data received
+	      
+	      String sentence = inFromUser.readLine();									//get user input
+	      sendData = sentence.getBytes();											//put input into the send array
+	      DatagramPacket sendPacket = 
+	    		  new DatagramPacket(sendData, sendData.length, IPAddress, 9876);	//make packet with data, address and port
+	      clientSocket.send(sendPacket);											//send the packet
+	      DatagramPacket receivePacket = 
+	    		  new DatagramPacket(receiveData, receiveData.length);				//make a packet to receive data
+	      clientSocket.receive(receivePacket);										//wait for and receive a response in the packet
+	      String modifiedSentence = new String(receivePacket.getData());			//extract the pay load
+	      System.out.println("FROM SERVER:" + modifiedSentence);					// print the pay load
+	      clientSocket.close();														//close the socket;
 	 }
 }
--- RPISensors/src/main/java/comms/UDPServer.java
+++ RPISensors/src/main/java/comms/UDPServer.java
@@ -12,21 +12,24 @@ public class UDPServer {
 	public static void main(String args[]) throws Exception
 	{
    		DatagramSocket serverSocket = new DatagramSocket(9876);
-        byte[] receiveData = new byte[1024];
-        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];											//Array for holding data received
+        byte[] sendData = new byte[1024];												//Array for holding data to be sent
         while(true)
         {
-              DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-              serverSocket.receive(receivePacket);
-              String sentence = new String( receivePacket.getData());
-              System.out.println("RECEIVED: " + sentence);
-              InetAddress IPAddress = receivePacket.getAddress();
-              int port = receivePacket.getPort();
-              String capitalizedSentence = sentence.toUpperCase();
-              sendData = capitalizedSentence.getBytes();
-              DatagramPacket sendPacket =
-              new DatagramPacket(sendData, sendData.length, IPAddress, port);
-              serverSocket.send(sendPacket);
+              DatagramPacket receivePacket = 
+            		  new DatagramPacket(receiveData, receiveData.length); 				//packet for receiving data
+              serverSocket.receive(receivePacket); 										//receive the data packet
+              String sentence = new String( receivePacket.getData()); 					//extract the data
+              System.out.println("RECEIVED: " + sentence); 								//output the data
+              InetAddress IPAddress = receivePacket.getAddress();						//record address it came from
+              int port = receivePacket.getPort();										//record port  it came from
+              
+              String capitalizedSentence = sentence.toUpperCase(); 						//modify the data
+              
+              sendData = capitalizedSentence.getBytes(); 								// add data to packet
+              DatagramPacket sendPacket = 
+            		  new DatagramPacket(sendData, sendData.length, IPAddress, port); 	//address packet
+              serverSocket.send(sendPacket); 											//reply by sending packet containing modified data
         }
 	}
 }
