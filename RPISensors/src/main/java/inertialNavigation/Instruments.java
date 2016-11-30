30b5f85be6ccd1d70f3e03f700342c0bcd0717e4
gjwo <grahamjwood@gmail.com>
2016-11-30 17:18:20 +0000

    major restructure of Instruments and Quaternion
    Angle manipulations moved into Quaternion, variables and access methods
    rationalised in Instruments. Update timestamp added

--- RPISensors/src/main/java/inertialNavigation/Instruments.java
+++ RPISensors/src/main/java/inertialNavigation/Instruments.java
@@ -1,5 +1,9 @@
 package inertialNavigation;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import dataTypes.Data3f;
 import dataTypes.TimestampedData3f;
 
@@ -9,59 +13,58 @@ import dataTypes.TimestampedData3f;
  */
 public class Instruments {
 	
-	//Fused  data from several sensors
-	private static float yaw = 0; //Yaw is the angle between SensorPackage x-axis and Earth magnetic North (or true North if corrected for local declination, looking down on the sensor positive yaw is counterclockwise.
-	private static float pitch = 0; //Pitch is angle between sensor x-axis and Earth ground plane, toward the Earth is positive, up toward the sky is negative.
-	private static float roll = 0; //Roll is angle between sensor y-axis and Earth ground plane, y-axis up is positive roll.
-	
-	private static float speed = 0;
-	
-	private static Data3f linearAcceleration = new Data3f(0,0,0);
-	
-	//Data computed from integrating sensor information
-	private static float heading = 0;
-	private static float attitude = 0;
-	private static float bank = 0;
-	
-	private static TimestampedData3f position = new TimestampedData3f(0,0,0);
+	//Time of last instrument update
+	private static Instant updatedTimestamp = Instant.now();
 	
 	//data from individual sensors
 	private static TimestampedData3f magnetometer = new TimestampedData3f(0,0,0);
 	private static TimestampedData3f accelerometer = new TimestampedData3f(0,0,0);
 	private static TimestampedData3f gyroscope = new TimestampedData3f(0,0,0);
 	
+	//Fused  data from several sensors
+	private static Quaternion quaternion;
+	private static Data3f TaitBryanAngles = new Data3f(0,0,0); //in radians
+	
+	//in degrees adjusted for location and yaw to read 0-360
+	private static float yaw = 0; 	//Yaw is the angle between SensorPackage x-axis and Earth magnetic North (or true North if corrected for local declination, looking down on the sensor positive yaw is counterclockwise.
+	private static float pitch = 0; //Pitch is angle between sensor x-axis and Earth ground plane, toward the Earth is positive, up toward the sky is negative.
+	private static float roll = 0; 	//Roll is angle between sensor y-axis and Earth ground plane, y-axis up is positive roll.
+									//Alternate names for the same things are heading, attitude and bank 
+	
+	private static Data3f linearAcceleration = new Data3f(0,0,0);
+	
 	// getters
+	public static Instant getTimestamp(){return updatedTimestamp;}
+	public static ZonedDateTime getDateTime() {return updatedTimestamp.atZone(ZoneId.of("GB"));}
 	public static float getYaw() {return yaw;}
 	public static float getPitch() {return pitch;}
 	public static float getRoll() {return roll;}
-	
-	public static float getSpeed() {return speed;}						//TBD
-	public static float getHeading() {return heading;}					//TBD
-	public static TimestampedData3f getPosition() {return position;}	//TBD
-	
+	public static float getHeading() {return getYaw();}	
+	public static float getAttitude() {return getPitch();}
+	public static float getBank() {return getRoll();}
+		
 	public static TimestampedData3f getMagnetometer() {return magnetometer;}
 	public static TimestampedData3f getAccelerometer() {return accelerometer;}
 	public static TimestampedData3f getGyroscope() {return gyroscope;}
 	public static TimestampedData3f getAngles(){return new TimestampedData3f(yaw,pitch,roll);}
 	
-	public static float getAttitude() {return attitude;}
-	public static float getBank() {return bank;}
-	
 	public static Data3f getLinearAcceleration() {return linearAcceleration;}
 
 	//Setters
-	public static void setBank(float bank) {Instruments.bank = bank;}
-	public static void setAttitude(float attitude) {Instruments.attitude = attitude;}
-	public static void setPitch(float pitch) {Instruments.pitch = pitch;}
-	public static void setHeading(float heading) {Instruments.heading = heading;}
-	public static void setPosition(TimestampedData3f position) {Instruments.position = position;}
 	public static void setMagnetometer(TimestampedData3f magnetometer) {Instruments.magnetometer = magnetometer;}
-	public static void setAccelerometer(TimestampedData3f accellerometer) {Instruments.accelerometer = accellerometer;}
+	public static void setAccelerometer(TimestampedData3f accelerometer) {Instruments.accelerometer = accelerometer;}
 	public static void setGyroscope(TimestampedData3f gyroscope) {Instruments.gyroscope = gyroscope;}
 
+	//public static void setYaw(float yaw) {Instruments.yaw = yaw;}
+	//public static void setHeading(float heading) {Instruments.yaw = heading;}
+	//public static void setAttitude(float attitude) {Instruments.pitch = attitude;}
+	//public static void setPitch(float pitch) {Instruments.pitch = pitch;}
+	//public static void setBank(float bank) {Instruments.roll = bank;}
+	//public static void setRoll(float roll) {Instruments.roll = roll;}
+	
 	public static void printInstruments()
 	{
-		
+		//TBD
 	}
 	
 	/**
@@ -83,26 +86,28 @@ public class Instruments {
 	 */
 	public static void updateInstruments(Quaternion q)
 	{		
-	    float a12 =   2.0f * (q.x * q.y + q.w * q.z);					// #KW L625 
-	    float a22 =   q.w * q.w + q.x * q.x - q.y * q.y - q.z * q.z;
-	    float a31 =   2.0f * (q.w * q.x + q.y * q.z);
-	    float a32 =   2.0f * (q.x * q.z - q.w * q.y);
-	    float a33 =   q.w * q.w - q.x * q.x - q.y * q.y + q.z * q.z;
+		updatedTimestamp = Instant.now();
+		quaternion = q;
+		TaitBryanAngles = q.toTaitBryanAngles();
 	    
-	    pitch = (float) -Math.asin(a32);					// #KW L630
-	    roll  = (float) Math.atan2(a31, a33);
-	    yaw   = (float) Math.atan2(a12, a22);
-	    pitch *= 180.0f / Math.PI; //radians to degrees		// #KW L633
-	    yaw   *= 180.0f / Math.PI; //radians to degrees		// #KW L634
-	    roll  *= 180.0f / Math.PI; //radians to degrees		// #KW L637
+	    yaw   = (float) Math.toDegrees(TaitBryanAngles.getX()); 	//radians to degrees		// #KW L634
+	    pitch = (float) Math.toDegrees(TaitBryanAngles.getY()); 	//radians to degrees		// #KW L633
+	    roll  = (float) Math.toDegrees(TaitBryanAngles.getZ()); 	//radians to degrees		// #KW L637
+
 	    // #KW L635 yaw   -= 13.8; // Declination at Danville, California is 13 degrees 48 minutes and 47 seconds on 2014-04-04
 	    yaw   += -44.0f/60.0f; // Declination at Letchworth England is minus O degrees and 44 Seconds on 2016-07-11
 	    if(yaw < 0) yaw   += 360.0f; // Ensure heading stays between 0 and 360
-	    heading = yaw;
-	    attitude = pitch;
-	    bank = roll;
+	    updatelinearAcceleration(quaternion);
+	}
+	
+	private static void updatelinearAcceleration(Quaternion q)
+	{
+	    float a31 =   2.0f * (q.w * q.x + q.y * q.z);
+	    float a32 =   2.0f * (q.x * q.z - q.w * q.y);
+	    float a33 =   q.w * q.w - q.x * q.x - q.y * q.y + q.z * q.z;
 	    linearAcceleration.setX( accelerometer.getX() + a31);
 	    linearAcceleration.setY(accelerometer.getY() + a32);
 	    linearAcceleration.setZ(accelerometer.getZ() - a33);
+
 	}
 }
--- RPISensors/src/main/java/inertialNavigation/Quaternion.java
+++ RPISensors/src/main/java/inertialNavigation/Quaternion.java
@@ -28,7 +28,7 @@ public class Quaternion
 	}
 	
 	/**
-	 * Quaternion	-	Constructor from yaw, pitch and roll
+	 * Quaternion	-	Constructor from yaw, pitch and roll (Eulerian angles)
 	 * @param yaw
 	 * @param pitch
 	 * @param roll
@@ -41,10 +41,10 @@ public class Quaternion
 		float t3 = (float)Math.sin(roll * 0.5f);
 		float t4 = (float)Math.cos(pitch * 0.5f);
 		float t5 = (float)Math.sin(pitch * 0.5f);
-		this.w = t0 * t2 * t4 + t1 * t3 * t5;
-		this.x = t0 * t3 * t4 - t1 * t2 * t5;
-		this.y = t0 * t2 * t5 + t1 * t3 * t4;
-		this.z = t1 * t2 * t4 - t0 * t3 * t5;		
+		w = t0 * t2 * t4 + t1 * t3 * t5;
+		x = t0 * t3 * t4 - t1 * t2 * t5;
+		y = t0 * t2 * t5 + t1 * t3 * t4;
+		z = t1 * t2 * t4 - t0 * t3 * t5;		
 	}
 
 	/**
@@ -59,25 +59,47 @@ public class Quaternion
 	public Quaternion(){this(0,0,0,0);}
 	
 	/**
-	 * toEulerianAngle	-	Convert w quaternion back into w Eulerian angle
-	 * @return
+	 * toEulerianAngle	-	Convert a quaternion back into a set of Eulerian angles
+	 * 	
+	 * @return	a class containing yaw pitch and roll
 	 */
-	public Data3f toEulerianAngle()
+	public Data3f toEulerianAngles()
 	{
-		float ysqr = this.y * this.y;
-		float t0 = -2.0f * (ysqr + this.z * this.z) + 1.0f;
-		float t1 = +2.0f * (this.x * this.y - this.w * this.z);
-		float t2 = -2.0f * (this.x * this.z + this.w * this.y);
-		float t3 = +2.0f * (this.y * this.z - this.w * this.x);
-		float t4 = -2.0f * (this.x * this.x + ysqr) + 1.0f;
+		float ysqr = y * y;
+		float t0 = -2.0f * (ysqr + z * z) + 1.0f;
+		float t1 = +2.0f * (x * y - w * z);
+		float t2 = -2.0f * (x * z + w * y);
+		float t3 = +2.0f * (y * z - w * x);
+		float t4 = -2.0f * (x * x + ysqr) + 1.0f;
 
-		t2 = t2 > 1.0f ? 1.0f : t2;
-		t2 = t2 < -1.0f ? -1.0f : t2;
+		t2 = t2 > 1.0f ? 1.0f : t2;		//Deal with singularity
+		t2 = t2 < -1.0f ? -1.0f : t2;	//Deal with singularity
+		
+		float yaw = (float)Math.atan2(t1, t0); 	//yaw
+		float pitch = (float)Math.asin(t2); 	//pitch
+		float roll = (float)Math.atan2(t3, t4); //roll
 
-		return new Data3f(	(float)Math.atan2(t1, t0), //yaw
-							(float)Math.sin(t2), //pitch
-							(float)Math.atan2(t3, t4)); //roll
-	}	
+		return new Data3f(yaw, pitch, roll);
+	}
+	
+	/**
+	 * toTaitBryanAngle	-	Convert a quaternion back into a set of Tait-Bryan angles
+	 * @return	-	structure of Yaw, Pitch and Roll in degrees
+	 */
+	public Data3f toTaitBryanAngles()
+	{
+	    float a12 =   2.0f * (x * y + w * z);					// #KW L625 
+	    float a22 =   w * w + x * x - y * y - z * z;
+	    float a31 =   2.0f * (w * x + y * z);
+	    float a32 =   2.0f * (x * z - w * y);
+	    float a33 =   w * w - x * x - y * y + z * z;
+	    
+	    float pitch = (float) -Math.asin(a32);					// #KW L630
+	    float roll  = (float) Math.atan2(a31, a33);
+	    float yaw   = (float) Math.atan2(a12, a22);
+	    
+	    return new Data3f(yaw,pitch,roll);
+	}
 	
 	/**
 	 * normalise	- normalise by dividing each value by the square root of the sum of the squares of the values
