b5e8013115b35a424f618f0776fa7340a4b48c30
MAWood <mattcow12@googlemail.com>
2016-12-06 21:20:42 +0000

    Tested drive system

--- RPISensors/src/main/java/main/MotorTest.java
+++ RPISensors/src/main/java/main/MotorTest.java
@@ -1,8 +1,10 @@
 package main;
 
+import com.pi4j.io.gpio.*;
 import devices.driveAssembly.DriveAssembly;
 import devices.driveAssembly.TankDriveAssembly;
-import devices.motors.DebugMotor;
+import devices.motors.DCMotor;
+import devices.motors.Motor;
 
 /**
  * RPISensors - main
@@ -10,14 +12,27 @@ import devices.motors.DebugMotor;
  */
 public class MotorTest
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
-        DriveAssembly DA = new TankDriveAssembly(new DebugMotor("left"),new DebugMotor("right"));
-        DA.setSpeed(1f);
-        DA.setDirection(180);
+        final GpioController gpio = GpioFactory.getInstance();
 
-        DA.setDirection(0);
-        DA.setDirection(270);
-        DA.setDirection(90);
+        final GpioPinDigitalOutput RA =
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Right motor A", PinState.LOW);
+        final GpioPinDigitalOutput RB =
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "Right motor B", PinState.LOW);
+        final GpioPinDigitalOutput LA =
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Left motor A", PinState.LOW);
+        final GpioPinDigitalOutput LB =
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Left motor B", PinState.LOW);
+
+        Motor left = new DCMotor(LA,LB);
+        Motor right = new DCMotor(RA,RB);
+
+        DriveAssembly DA = new TankDriveAssembly(left,right);
+        Thread.sleep(100);
+        DA.setDirection(0f);
+        DA.setSpeed(1f);
+        Thread.sleep(1000);
+        DA.stop();
     }
 }
