af3e9c8b8f70a4df094808cb64eddd43b6ec5136
MAWood <mattcow12@googlemail.com>
2016-12-07 01:14:11 +0000

    Testing drive system

--- RPISensors/src/main/java/main/MotorTest.java
+++ RPISensors/src/main/java/main/MotorTest.java
@@ -1,6 +1,7 @@
 package main;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import devices.driveAssembly.DriveAssembly;
 import devices.driveAssembly.TankDriveAssembly;
 import devices.motors.DCMotor;
@@ -10,8 +11,9 @@ import devices.motors.Motor;
  * RPISensors - main
  * Created by MAWood on 03/12/2016.
  */
-public class MotorTest
+public class MotorTest extends Thread
 {
+
     public static void main(String[] args) throws InterruptedException
     {
         final GpioController gpio = GpioFactory.getInstance();
@@ -25,14 +27,30 @@ public class MotorTest
         final GpioPinDigitalOutput LB =
                 gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Left motor B", PinState.LOW);
 
+        final GpioPinDigitalInput RH1 =
+                gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "RH1", PinPullResistance.PULL_DOWN);
+        final GpioPinDigitalInput RH2 =
+                gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, "RH2", PinPullResistance.PULL_DOWN);
+
+        RH1.setShutdownOptions(true);
+        RH2.setShutdownOptions(true);
+
+        RH1.addListener((GpioPinListenerDigital) event ->
+        {
+            // display pin state on console
+            System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+        });
+        RH2.addListener((GpioPinListenerDigital) event ->
+        {
+            // display pin state on console
+            System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+        });
+
         Motor left = new DCMotor(LA,LB);
         Motor right = new DCMotor(RA,RB);
 
-        DriveAssembly DA = new TankDriveAssembly(left,right);
-        Thread.sleep(100);
-        DA.setDirection(0f);
-        DA.setSpeed(1f);
-        Thread.sleep(1000);
-        DA.stop();
+        DriveAssembly driveAssembly = new TankDriveAssembly(left,right);
+
+
     }
 }
