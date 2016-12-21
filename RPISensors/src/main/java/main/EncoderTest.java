beebba549ffec48c9655b1e2434d721b2ed1327a
MAWood <mattcow12@googlemail.com>
2016-12-21 16:55:31 +0000

    Testing encoder

--- RPISensors/src/main/java/main/EncoderTest.java
+++ RPISensors/src/main/java/main/EncoderTest.java
@@ -3,35 +3,73 @@ package main;
 import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
 /**
  * RPISensors - main
  * Created by MAWood on 03/12/2016.
  */
-public class EncoderTest extends Thread
+public class EncoderTest
 {
 
-    public static void main(String[] args) throws InterruptedException
+    int one = 0;
+    int two = 0;
+
+    int lastOne = 0;
+    int lastTwo = 0;
+
+    Instant start;
+
+    Direction direction;
+
+    enum Direction
+    {
+        FORWARDS,
+        BACKWARDS
+    }
+
+    public EncoderTest() throws InterruptedException
     {
         final GpioController gpio = GpioFactory.getInstance();
 
 
         final GpioPinDigitalInput RH1 =
-                gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "RH1", PinPullResistance.PULL_DOWN);
+                gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, "RH1", PinPullResistance.PULL_DOWN);
         final GpioPinDigitalInput RH2 =
-                gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, "RH2", PinPullResistance.PULL_DOWN);
+                gpio.provisionDigitalInputPin(RaspiPin.GPIO_14, "RH2", PinPullResistance.PULL_DOWN);
 
         RH1.setShutdownOptions(true);
         RH2.setShutdownOptions(true);
+        direction = Direction.FORWARDS;
 
         RH1.addListener((GpioPinListenerDigital) event ->
         {
             // display pin state on console
-            System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            one++;
+            if(lastTwo == two) direction = Direction.FORWARDS;
+            lastTwo = two;
+            //System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
         });
         RH2.addListener((GpioPinListenerDigital) event ->
         {
+            two++;
+            if(lastOne == one) direction = Direction.BACKWARDS;
+            lastOne = one;
             // display pin state on console
-            System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            //System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
         });
+        while(true)
+        {
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println("Direction: " + direction.name() + " A: " + one + " B: " + two);
+            one = 0;
+            two = 0;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        new EncoderTest();
     }
 }
