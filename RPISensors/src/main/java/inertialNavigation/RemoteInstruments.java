3000c25550171d3f72db66bf8f4b11e87a15ea7c
gjwo <grahamjwood@gmail.com>
2016-12-21 09:37:19 +0000

    Add all Instrument getters

--- RPISensors/src/main/java/inertialNavigation/RemoteInstruments.java
+++ RPISensors/src/main/java/inertialNavigation/RemoteInstruments.java
@@ -1,9 +1,12 @@
 package inertialNavigation;
 
 import dataTypes.Data3f;
+import dataTypes.TimestampedData3f;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.Instant;
+import java.time.ZonedDateTime;
 
 /**
  * NavigationDisplay - inertialNavigation
@@ -12,4 +15,20 @@ import java.rmi.RemoteException;
 public interface RemoteInstruments extends Remote
 {
     Data3f getTaitBryanAnglesD() throws RemoteException;
+	public Instant getTimestamp() throws RemoteException;
+	public ZonedDateTime getDateTime() throws RemoteException;
+	public float getYaw() throws RemoteException;
+	public float getPitch() throws RemoteException;
+	public float getRoll() throws RemoteException;
+	public float getHeading() throws RemoteException;
+	public float getAttitude() throws RemoteException;
+	public float getBank() throws RemoteException;
+	public Instant getUpdatedTimestamp() throws RemoteException;
+	public Quaternion getQuaternion() throws RemoteException;
+	public Data3f getTaitBryanAnglesR() throws RemoteException;
+	public TimestampedData3f getMagnetometer() throws RemoteException;
+	public TimestampedData3f getAccelerometer() throws RemoteException;
+	public TimestampedData3f getGyroscope() throws RemoteException;
+	public TimestampedData3f getAngles() throws RemoteException;
+	public Data3f getLinearAcceleration() throws RemoteException;
 }
