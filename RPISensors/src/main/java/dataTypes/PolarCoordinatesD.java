f089826d45bdd15ec3cad5a8ff57a29f9e0c0c7d
gjwo <grahamjwood@gmail.com>
2017-02-02 11:20:33 +0000

    Added getters and setters

--- RPISensors/src/main/java/dataTypes/PolarCoordinatesD.java
+++ RPISensors/src/main/java/dataTypes/PolarCoordinatesD.java
@@ -9,6 +9,7 @@ public class PolarCoordinatesD
     private double theta;
     private double r;
 
+
     /**
      * PolarCoordinatesD    -   Constructor with user values
      * @param theta         -   angle in radians
@@ -25,6 +26,13 @@ public class PolarCoordinatesD
     public PolarCoordinatesD(){
         this(0,0);
     }
+
+    // getters
+    public double getTheta() {return theta;}
+    public double getR() {return r;}
+    //setters
+    public void setR(double r) {this.r = r;}
+    public void setTheta(double theta) {this.theta = theta;}
 
     public String toString(){
         return String.format("theta: %1.3f r: %1.3f",theta,r);
